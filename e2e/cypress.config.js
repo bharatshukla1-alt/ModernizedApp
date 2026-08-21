a07e52f7eb681f69b71bddab9c2dd2336a54bd66
bharatshukla1-alt <bharat.shukla1@tcs.com>
2026-08-21 10:44:08 +0530

    Add e2e/cypress.config.js

---
+++ e2e/cypress.config.js
@@ -0,0 +1,17 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:4200',
+    specPattern: 'cypress/e2e/**/*.cy.js',
+    supportFile: 'cypress/support/e2e.js',
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    video: false,
+    screenshotOnRunFailure: true,
+    defaultCommandTimeout: 8000,
+    setupNodeEvents(on, config) {
+      return config;
+    }
+  }
+});
